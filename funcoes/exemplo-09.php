2aa103d70410e1b7f7ccdc7539bb2a7c83b96d52
Enzo Teixeira <enzo_jesus@icloud.com>
2018-02-07 17:53:36 -0300

    Complemento do código

--- funcoes/exemplo-09.php
+++ funcoes/exemplo-09.php
@@ -6,10 +6,48 @@ $hierarquia = array(
 		'subordinados'=>array(
 			//Início: Diretor Comercial
 			array(
-				'nome_cargo'=>'Diretor Comercial'
+				'nome_cargo'=>'Diretor Comercial',
+				'subordinados'=>array(
+					//Início: Gerente de Vendas
+					array(
+						'nome_cargo'=>'Gerente de Vendas'
+					)
+					//Início: Gerente de Vendas
+				)
 			),
-			//Início: Diretor Comercial
-			array()
+			//Término: Diretor Comercial
+			//Início: Diretor Financeiro
+			array(
+				'nome_cargo'=>'Diretor Financeiro',
+				'subordinados'=>array(
+					//Início: Gerente de Contas a Pagar
+					array(
+						'nome_cargo'=>'Gerente de Contas a Pagar',
+						'subordinados'=>array(
+							//Início: Supervisor de Pgamentos
+							array(
+								'nome_cargo'=>'Supervisor de Pagamentos'
+							)
+							//Término: Supervisor de Pagamentos
+						)
+					)
+					//Término: Gerente de Contas a Pagar
+					//Início: Gerente de Compras
+					array(
+						'nome_cargo'=>'Gerente de Compras',
+						'subordinados'=>array(
+							//Início: Supervisor de Suprimentos
+							array(
+								'nome_cargo'=>'Supervisor de Suprimentos'
+								)
+							)
+							//Término: Supervisor de Suprimentos
+						)
+					)
+					// Término: Gerente de Compras
+				)
+			)
+			//Término: Diretor Financeiro
 		)
 	)
 );
